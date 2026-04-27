e38f092cf5745e53802798e5c05012a445330f4d
Noah Amunder <noaamu@edu.umea.se>
2026-04-27 13:38:02 +0200

    memory

---
+++ docs/_app/immutable/chunks/D4XIf5ll.js
@@ -0,0 +1,1 @@
+import{T as a}from"./c99ZpK2I.js";a();
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,18 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+
+	let {base} = '$app/paths';
 </script>
 
 <nav>
-	<a href="/">Home Page</a>
-	<a href="/photoapp">Photo App</a>
-	<a href="register"> Register </a>
-	<a href="loggin"> Logga in </a>
-	<a href="/shoppinglist"> Shopping list </a>
-	<a href="/memory"> Memory </a>
-	<a href="/test"> Test </a>
+	<a href="{base}/">Home Page</a>
+	<a href="{base}/photoapp">Photo App</a>
+	<a href="{base}/register"> Register </a>
+	<a href="{base}/loggin"> Logga in </a>
+	<a href="{base}/shoppinglist"> Shopping list </a>
+	<a href="{base}/memory"> Memory </a>
+	<a href="{base}/test"> Test </a>
 
 </nav>
 
